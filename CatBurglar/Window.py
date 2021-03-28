f020a522b74830adfea3e4a4393771cfa2d1a050
markjoshua12 <mark.joshua121001@gmail.com>
2021-03-28 17:37:38 +0800

    Created Player class

--- CatBurglar/Window.py
+++ CatBurglar/Window.py
@@ -1,6 +1,9 @@
 
 import arcade
 
+from CatBurglar.input.KeyHandler import KeyHandler
+from CatBurglar.entity.Player import Player
+
 WIDTH = 800
 HEIGHT = 450
 
@@ -15,9 +18,23 @@ class Window(arcade.Window):
 
     def setup(self):
         self.sprite_list = arcade.SpriteList()
+        self.key_handler = KeyHandler()
+
+        self.player = Player(self.key_handler)
+        self.player.set_position(32, 32)
+
+        self.sprite_list.append(self.player)
 
     def on_update(self, delta_time):
-        pass
+        self.sprite_list.update()
 
     def on_draw(self):
         arcade.start_render()
+
+        self.sprite_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        self.key_handler.on_key_press(key, modifiers)
+
+    def on_key_release(self, key, modifiers):
+        self.key_handler.on_key_release(key, modifiers)
---
+++ CatBurglar/entity/Player.py
@@ -0,0 +1,41 @@
+
+import arcade
+
+PLAYER_WIDTH = 16
+PLAYER_HEIGHT = 32
+
+GRAVITY = 1
+
+class Player(arcade.SpriteSolidColor):
+
+    def __init__(self, key_handler):
+        super().__init__(PLAYER_WIDTH, PLAYER_HEIGHT, arcade.color.AQUA)
+
+        self.key_handler = key_handler
+
+        self.move_speed = 1
+        self.jump_speed = 4
+
+    def update(self):
+
+        self.change_x *= .9
+        self.change_y *= .94
+        
+        if self.key_handler.is_pressed("LEFT"):
+            self.change_x -= self.move_speed
+
+        if self.key_handler.is_pressed("RIGHT"):
+            self.change_x += self.move_speed
+
+        if self.key_handler.is_pressed("UP"):
+            self.change_y += self.jump_speed
+
+        if self.key_handler.is_pressed("DOWN"):
+            self.change_y -= self.move_speed
+
+        super().update()
+
+        if self.bottom < 0:
+            self.bottom = 0
+        else:
+            self.change_y -= GRAVITY
