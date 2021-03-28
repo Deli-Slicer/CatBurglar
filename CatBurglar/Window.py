20e5a9ede5c2ce07058689af96d79cb670494fb0
markjoshua12 <mark.joshua121001@gmail.com>
2021-03-29 03:15:09 +0800

    Added Camera functionality

--- CatBurglar/Window.py
+++ CatBurglar/Window.py
@@ -2,23 +2,34 @@
 import arcade
 
 from CatBurglar.input.KeyHandler import KeyHandler
+from CatBurglar.graphics.Camera import Camera
 from CatBurglar.entity.Player import Player
 
 WIDTH = 800
 HEIGHT = 450
 
+MIN_WIDTH = 160
+MIN_HEIGHT = 90
+
 TITLE = "Cat Burglar"
 
-RESIZABLE = False
+RESIZABLE = True
 
 class Window(arcade.Window):
 
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, TITLE, resizable=RESIZABLE)
 
+        self.center_window()
+
+        self.set_min_size(MIN_WIDTH, MIN_HEIGHT)
+
     def setup(self):
         self.sprite_list = arcade.SpriteList()
         self.key_handler = KeyHandler()
+
+        self.camera = Camera(self.width, self.height, True)
+        self.zoom_speed = .95
 
         self.player = Player(self.key_handler)
         self.player.set_position(32, 32)
@@ -28,8 +39,17 @@ class Window(arcade.Window):
     def on_update(self, delta_time):
         self.sprite_list.update()
 
+        if self.key_handler.is_pressed("ZOOM_IN"):
+            self.camera.zoom(self.zoom_speed)
+        elif self.key_handler.is_pressed("ZOOM_OUT"):
+            self.camera.zoom(1 / self.zoom_speed)
+
+        self.camera.scroll_to(self.player.center_x, self.player.center_y)
+
     def on_draw(self):
         arcade.start_render()
+
+        self.camera.set_viewport()
 
         self.sprite_list.draw()
 
@@ -38,3 +58,6 @@ class Window(arcade.Window):
 
     def on_key_release(self, key, modifiers):
         self.key_handler.on_key_release(key, modifiers)
+
+    def on_resize(self, width, height):
+        self.camera.resize(width, height)
