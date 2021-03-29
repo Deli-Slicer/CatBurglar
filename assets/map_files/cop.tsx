aa195236b0f990f9774493204048f2e53557677b
markjoshua12 <mark.joshua121001@gmail.com>
2021-03-29 22:15:47 +0800

    Added tmx test files

---
+++ assets/map_files/cop.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="still_left_0" tilewidth="16" tileheight="32" tilecount="1" columns="1">
+ <image source="../cop/still_left_0.png" width="16" height="32"/>
+</tileset>
